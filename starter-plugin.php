0025978d104b9f7592d119ac36e22c2c2210a0e3
Matty <hello@matty.co.za>
2014-09-20 09:21:48 +0200

    Adjusts the plugin header comment to differentiate the plugin URI and author URI.

--- starter-plugin.php
+++ starter-plugin.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Plugin Name: Starter Plugin
- * Plugin URI: http://matty.co.za/
+ * Plugin URI: http://domain.com/starter-plugin/
  * Description: Hey there! I'm your new starter plugin.
  * Version: 1.0.0
  * Author: Matty
- * Author URI: http://matty.co.za/
+ * Author URI: http://domain.com/
  * Requires at least: 4.0.0
  * Tested up to: 4.0.0
  *
